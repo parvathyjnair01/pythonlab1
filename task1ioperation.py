d94f6299126daa72ebee8f386a088322cb3d4744
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-18 14:29:06 +0530

    Add files via upload

---
+++ task1ioperation.py
@@ -0,0 +1,8 @@
+'''Author:Parvathy J
+Date:18-10-2024
+Description:program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+Hello,John!You are 25 years old'''
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello,",name+"!you are",age,"years old")
