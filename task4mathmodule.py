edd1d3314fa84c33b75156b1b8b011217f2945f3
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-18 16:20:11 +0530

    Add files via upload

---
+++ task4mathmodule.py
@@ -0,0 +1,8 @@
+import math
+number=int(input("Enter a number:"))
+square_root=math.sqrt(number)
+print("Square root of ",number,":",square_root)
+factorial=math.factorial(number)
+print("Factorial of ",number,":",factorial)
+power=math.pow(number,2)
+print(number,"raised to power 2",":",power)
