94dab6f03fce5aa440cf9cc08e02f9a5adf308e0
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-18 15:10:09 +0530

    Add files via upload

---
+++ task2string.py
@@ -0,0 +1,11 @@
+'''Author:Parvathy J Nair
+Date:18-10-2024
+Description:program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+str_var="Hello, World!"
+str_another_var=(str_var[7:12])
+print(str_another_var)
+final_str=str_another_var+ " Everyone!"
+print(final_str)
