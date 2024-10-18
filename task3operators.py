cbc265f0279574641e1c9f41d8596d894107b940
parvathyjnair01 <parvathyj06@gmail.com>
2024-10-18 15:57:01 +0530

    Add files via upload

---
+++ task3operators.py
@@ -0,0 +1,12 @@
+'''Author:Parvathy J Nair
+Date:18-10-2024
+Description:program that demonstrates the usage of arithmetic, comparison, and logical operators.
+Perform a few operations and print the results.'''
+
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print("Sum=",number1+number2,"Division=",number1/number2)
+print("Is number1 greater than number2?:",number1>number2)
+print("Are number1 and number2 equal?:",number1==number2)
+print("Logical And:",number1>0 and number2>0)
+print("Logical Or:",number1>0 or number2>1)sssssssss
